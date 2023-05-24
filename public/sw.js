58d96621be152966c8f82c129e6d7d508d80cbaa
elCamilet <elcamilet@gmail.com>
2023-05-25 01:23:52 +0200

    added serviceWorker

---
+++ public/sw.js
@@ -0,0 +1,43 @@
+const CACHE_NAME = `SuenaTono`;
+
+// Use the install event to pre-cache all initial resources.
+self.addEventListener('install', event => {
+  event.waitUntil((async () => {
+    const cache = await caches.open(CACHE_NAME);
+    cache.addAll([
+      '/',
+      '/index.html',
+      '/index.js',
+      '/App.js',
+      '/App.css',
+      '/audio/april_showers.mp3',
+      '/audio/cant_stop_me.mp3',
+      '/audio/faidherbe_square.mp3',
+      '/static/js/bundle.js',
+      '/manifest.json'
+    ]);
+  })());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith((async () => {
+    const cache = await caches.open(CACHE_NAME);
+
+    // Get the resource from the cache.
+    const cachedResponse = await cache.match(event.request);
+    if (cachedResponse) {
+      return cachedResponse;
+    } else {
+        try {
+          // If the resource was not in the cache, try the network.
+          const fetchResponse = await fetch(event.request);
+
+          // Save the resource in the cache and return it.
+          cache.put(event.request, fetchResponse.clone());
+          return fetchResponse;
+        } catch (e) {
+          // The network failed.
+        }
+    }
+  })());
+});
