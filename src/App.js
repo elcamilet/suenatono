396c41120ccea4ec2f57df760dc6098944cb3688
elCamilet <elcamilet@gmail.com>
2023-06-13 00:08:12 +0200

    Segunda Contienda

--- src/App.js
+++ src/App.js
@@ -15,21 +15,27 @@ function App() {
 
   const LS_File = localStorage.getItem("audioFile");
   const LS_Label = localStorage.getItem("audioLabel");
+  const LS_Start = localStorage.getItem("audioStart");
   if (!LS_File) {
     localStorage.setItem("audioFile", "/audio/april_showers.mp3");
   }
   if (!LS_Label) {
     localStorage.setItem("audioLabel", "April Showers");
   }
+  if (!LS_Start) {
+    localStorage.setItem("audioStart", "0");
+  }
   const [audioFile, setAudioFile] = useState(LS_File);
   const [audioLabel, setAudioLabel] = useState(LS_Label);
-
+  const [startAt, setStartAt] = useState(LS_Start);
 
   const playAudio = (i) => {
     setAudioFile(audioFiles[i].value);
     setAudioLabel(audioFiles[i].label);
+    setStartAt("0");
     localStorage.setItem("audioFile", audioFiles[i].value);
     localStorage.setItem("audioLabel", audioFiles[i].label);
+    localStorage.setItem("audioStart", startAt);
   }
   const handleAudioChange = (e) => {
     const i = audioFiles.findIndex(el => el.value === e.target.value);
@@ -48,6 +54,10 @@ function App() {
     playAudio(i);    
   };
 
+  const handleListen = (e) => {
+    setStartAt(e.target.currentTime);
+  }
+
   const itemTemplate = (option, index) => {
     const isSelected = option.value === audioFile;
     const className = isSelected ? "active" : "";
@@ -58,14 +68,15 @@ function App() {
     );
   };
 
-    
-    return (
-      <div className="App">
+  fetch(audioFile);
+
+  return (
+    <div className="App">
       <header className="App-header">
         <h1>SuenaTono</h1>
       </header>      
       <div className="container">
-        <AudioPlayer src={audioFile} header={<h1>{audioLabel}</h1>} showSkipControls={true} onClickPrevious={handlePreviousSong} onClickNext={handleNextSong} customAdditionalControls={[]} customVolumeControls={[]} />
+        <AudioPlayer id="suenaTono" src={audioFile} preload="auto" onListen={e => handleListen(e)} header={<h1>{audioLabel}</h1>} showSkipControls={true} onClickPrevious={handlePreviousSong} onClickNext={handleNextSong} customAdditionalControls={[]} customVolumeControls={[]} />
         <ListBox value={audioFiles[0]} options={audioFiles} onChange={handleAudioChange} optionLabel="label" optionValue="value" itemTemplate={itemTemplate} />
       </div>
       <div className="footer">
